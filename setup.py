47954a148a24de5675a70985c4a2de747eda8ba7
Ondrej Sika <ondrej@ondrejsika.com>
2013-10-18 11:27:00 +0200

    update gui, v.1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "massresize",
-    version = "1.1.0",
+    version = "1.2.0",
     url = 'https://github.com/sikaondrej/massresize/',
     license = 'MIT',
     description = "Mass image resize",
